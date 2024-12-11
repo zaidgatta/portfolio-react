b8f3b933ca59634ee68e96f15d5cd626e5f425a9
zaidgatta <135795549+zaidgatta@users.noreply.github.com>
2024-12-11 15:18:24 +0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Graphic Designer" ];
+  const toRotate = [ "MERN Developer", "Graphic Designer", "Artist" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,7 +56,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Zaid Gatta`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Graphic Designer" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Hi! I'm Zaid Gatta`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "MERN Developer", "Graphic Designer", "Artist" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Hey there! I'm Zaid Gatta, a versatile graphic designer and web developer. I love creating visually stunning designs and user-friendly websites. Let's collaborate and turn your ideas into reality!</p>
                  {/* <button onClick={() => console.log('connect')}>Download my CV <ArrowRightCircle size={25} className="rotate-arrow" /></button>*/}
               </div>}
